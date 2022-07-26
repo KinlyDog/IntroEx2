7438d18e51e4efcb74e40644a18b50fb69fb466b
Otryaskin Andrey <kinlydog@gmail.com>
2022-07-26 21:12:00 +1000

    Fix bugs

--- src/Level1.java
+++ src/Level1.java
@@ -1,9 +1,14 @@
 public class Level1 {
     public static int odometer(int[] oksana) {
         int sum = 0;
+        int x;
 
         for (int i = 0; i < oksana.length - 1; i += 2) {
-            int x = oksana[i] * oksana[i + 1];
+            if (i == 0) {
+                x = oksana[i] * oksana[i + 1];
+            } else
+                x = oksana[i] * (oksana[i + 1] - oksana[i - 1]);
+
             sum += x;
         }
 
