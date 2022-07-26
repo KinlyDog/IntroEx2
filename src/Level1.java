40e2ac84a72855f0cf2d87f989c8866b216c5a44
Otryaskin Andrey <kinlydog@gmail.com>
2022-07-26 20:36:43 +1000

    Complete project

---
+++ src/Level1.java
@@ -0,0 +1,12 @@
+public class Level1 {
+    public static int odometer(int[] oksana) {
+        int sum = 0;
+
+        for (int i = 0; i < oksana.length - 1; i += 1) {
+            int x = oksana[i] * oksana[i + 1];
+            sum += x;
+        }
+
+        return sum;
+    }
+}
